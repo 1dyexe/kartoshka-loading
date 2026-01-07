aa90b8176c272acc2589ef05e60b28b6b8ce4470
1dyexe <indyvalentino@gmail.com>
2026-01-07 02:36:31 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+/kartoshka_loading/
+│
+├─ index.html       ← HTML principal
+├─ script.js        ← JS séparé
+├─ style.css        ← CSS séparé (optionnel mais propre)
+├─ music.mp3        ← musique de fond
+├─ logo.jpg         ← logo du serveur
+├─ bg1.jpg          ← fond 1
+├─ bg2.jpg          ← fond 2
+├─ bg3.jpg          ← fond 3
